0901f359649936be8ad115d227b52d00b4e18009
wanggaowan <wanggaowan@163.com>
2018-10-30 22:34:09 +0800

    熟悉container各属性含义和使用规则

--- lib/page/basic_widget/container.dart
+++ lib/page/basic_widget/container.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 import '../../common/util/screen_utils.dart';
@@ -17,11 +18,51 @@ class _ContainerPageState extends State<ContainerPage> {
         title: Text(ContainerPage._title),
       ),
       body: Center(
+        //heightFactor和widthFactor如果不指定，则它会尽可能大，
+        //以让child能够在可用区间内居中
+        //如果指定，则Center的宽就是child 的宽 * widthFactor
+        //Center的高就是child 的高 * heightFactor
+        //heightFactor: 4,
+        //widthFactor: 3,
+        //当Text文本横向能够完全绘制出来时，即使纵向高度不够，
+        // 文本也会被完全绘制出来，高度不会出现裁剪，
+        // 只有当横向不能够完全绘制出来时，纵向高度不够时才会被裁剪，猜测是Bug!
         child: new Container(
-          color: Colors.blueAccent,
-          width: ScreenUtil.scaleWidth(200),
-          height: ScreenUtil.scaleHeight(200),
-        ),
+          //约束宽高
+            width: ScreenUtil.scaleWidth(600),
+            height: ScreenUtil.scaleHeight(300),
+            //约束宽高最大值和最小值，如果width和height值不在此约束值范围之间，则无效
+            constraints: BoxConstraints.loose(
+                Size(double.infinity, ScreenUtil.scaleHeight(800))),
+            alignment: Alignment.center,
+            child: Text(
+              'Hello World',
+              style: Theme.of(context)
+                  .textTheme
+                  .display1
+                  .copyWith(color: Colors.red),
+              maxLines: 1,
+              locale: Locale('cn', 'zh'),
+              //展示不下时省略模式
+              overflow: TextOverflow.ellipsis,
+            ),
+            //背景样式，指定这个就不能指定color，反之亦然
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image:
+                NetworkImage('https://www.zybuluo.com/static/img/logo.png'),
+                centerSlice: Rect.fromLTRB(0.0, 0.0, 600.0, 600.0),
+              ),
+            ),
+            foregroundDecoration: BoxDecoration(
+                borderRadius: BorderRadius.all(
+                    Radius.circular(ScreenUtil.scaleWidth(20))),
+                border: Border.all(
+                    width: ScreenUtil.scaleWidth(2),
+                    color: Colors.red.shade700,
+                    style: BorderStyle.solid)),
+            //变换，缩放、旋转、平移
+            transform: Matrix4.rotationZ(-math.pi / 4)),
       ),
     );
   }
